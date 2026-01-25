93042eb9d4d6c1cbe86debe6fc698798801b06e1
Michael MACHADO <michael.machado.reino@gmail.com>
2026-01-25 23:10:48 +0100

    Fix molecule tests and move to virtualbox

--- molecule/default/tests/test_role_nfs_server.py
+++ molecule/default/tests/test_role_nfs_server.py
@@ -1,8 +1,23 @@
-def test_docker_is_installed(host):
+def test_nfs_package_is_installed(host):
     package = host.package("nfs-kernel-server")
     assert package.is_installed
 
-def test_docker_service(host):
+
+def test_nfs_service(host):
     service = host.service("nfs-kernel-server")
     assert service.is_running
     assert service.is_enabled
+
+
+def test_exports_file(host):
+    exports = host.file("/etc/exports")
+    assert exports.exists
+    assert exports.contains("/mnt/data")
+
+
+def test_default_export_directory(host):
+    directory = host.file("/mnt/data")
+    assert directory.exists
+    assert directory.is_directory
+    assert directory.user == "nobody"
+    assert directory.group == "nogroup"
